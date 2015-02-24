e4a89ff7d5080122e9466a83afe57ce05dff5209
Chris Hall <followingthepath@gmail.com>
2015-02-25 10:50:30 +1300

    adding further sub_space accessor methods, refactoring existing get_block to use them

--- src/space.h
+++ src/space.h
@@ -29,32 +29,75 @@ struct sub_space {
      */
     block * get_block(int block_x, int block_y, int block_z);
 
+    /* returns the chunk containing the block denotated by (x, y, z)
+     * or null
+     */
+    chunk * get_chunk_containing(int block_x, int block_y, int block_z);
+
+    /* returns the chunk corresponding to the chunk coordinates (x, y, z)
+     * note this is NOT using block coordinates
+     */
+    chunk * get_chunk(int chunk_x, int chunk_y, int chunk_z);
 };
 
+/* returns a block or null
+ * finds the block at the position (x,y,z) within
+ * the whole sub_space
+ * will move across chunks
+ */
 template <int N>
 block *
 sub_space<N>::get_block(int block_x, int block_y, int block_z)
 {
+    /* Within Block coordinates */
+    int wb_x, wb_y, wb_z;
+
+    chunk *c;
+
+    wb_x = block_x % CHUNK_SIZE;
+
+    wb_y = block_y % CHUNK_SIZE;
+
+    wb_z = block_z % CHUNK_SIZE;
+
+    c = this->get_chunk_containing(block_x, block_y, block_z);
+    if( ! c )
+        return 0;
+
+    return c->get_block(wb_x, wb_y, wb_z);
+}
+
+/* returns the chunk containing the block denotated by (x, y, z)
+ * or null
+ */
+template <int N>
+chunk *
+sub_space<N>::get_chunk_containing(int block_x, int block_y, int block_z)
+{
     int chunk_x, chunk_y, chunk_z;
 
     chunk_x = block_x / CHUNK_SIZE;
-    block_x = block_x % CHUNK_SIZE;
 
     chunk_y = block_y / CHUNK_SIZE;
-    block_y = block_y % CHUNK_SIZE;
 
     chunk_z = block_z / CHUNK_SIZE;
-    block_z = block_z % CHUNK_SIZE;
 
-    /* check bounds */
-    if( chunk_x >= N          ||
-        block_x >= CHUNK_SIZE ||
-        chunk_y >= N          ||
-        block_y >= CHUNK_SIZE ||
-        chunk_z >= N          ||
-        block_z >= CHUNK_SIZE )
+    return this->get_chunk(chunk_x, chunk_y, chunk_z);
+}
+
+/* returns the chunk corresponding to the chunk coordinates (x, y, z)
+ * note this is NOT using block coordinates
+ */
+template <int N>
+chunk *
+sub_space<N>::get_chunk(int chunk_x, int chunk_y, int chunk_z)
+{
+    if( chunk_x >= N ||
+        chunk_y >= N ||
+        chunk_z >= N )
         return 0;
 
-    return this->chunks.contents[chunk_x][chunk_y][chunk_z].get_block(block_x, block_y, block_z);
+    return &( this->chunks.contents[chunk_x][chunk_y][chunk_z] );
 }
+
 
