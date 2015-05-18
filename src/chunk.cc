306ca3bef21ce2ab725f1bd0b0890f676a80f2cd
Caleb Anderson <robotrising@gmail.com>
2015-05-18 14:50:35 -0600

    Memory: Initialize memory in spots that have presented problems. No doubt more remain.

--- src/chunk.cc
+++ src/chunk.cc
@@ -1,6 +1,25 @@
 #include "chunk.h"
 
 
+chunk::chunk()
+{
+    for (unsigned int z = 0; z < CHUNK_SIZE; ++z)
+    {
+        for (unsigned int y = 0; y < CHUNK_SIZE; ++y)
+        {
+            for (unsigned int x = 0; x < CHUNK_SIZE; ++x)
+            {
+                block *block = this->blocks.get(x, y, z);
+                block->type = block_empty;
+                for (int s = 0; s < face_count; ++s)
+                {
+                    block->surfs[s] = surface_none;
+                }
+            }
+        }
+    }
+}
+
 block *
 chunk::get_block(unsigned int x, unsigned int y, unsigned int z)
 {
