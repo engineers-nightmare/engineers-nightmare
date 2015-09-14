8b5021ef6f5f7e23cb047294a0df9d99f7b1408b
Caleb Anderson <robotrising@gmail.com>
2015-09-13 20:59:09 -0600

    component: add power provider component

---
+++ src/component/power_provider_component.cc
@@ -0,0 +1,63 @@
+#include <algorithm>
+#include "../memory.h"
+#include "power_provider_component.h"
+
+void
+power_provider_component_manager::create_component_instance_data(unsigned count) {
+    if (count <= buffer.allocated)
+        return;
+
+    component_buffer new_buffer;
+    power_instance_data new_pool;
+
+    size_t size = sizeof(c_entity) * count;
+    size = sizeof(unsigned) * count + align_size<unsigned>(size);
+    size = sizeof(unsigned) * count + align_size<unsigned>(size);
+    size += alignof(c_entity);  // for worst-case misalignment of initial ptr
+
+    new_buffer.buffer = malloc(size);
+    new_buffer.num = buffer.num;
+    new_buffer.allocated = count;
+    memset(new_buffer.buffer, 0, size);
+
+    new_pool.entity = align_ptr((c_entity *)new_buffer.buffer);
+    new_pool.max_provided = align_ptr((unsigned *)(new_pool.entity + count));
+    new_pool.provided = align_ptr((unsigned *)(new_pool.max_provided + count));
+
+    memcpy(new_pool.entity, instance_pool.entity, buffer.num * sizeof(c_entity));
+    memcpy(new_pool.max_provided, instance_pool.max_provided, buffer.num * sizeof(unsigned));
+    memcpy(new_pool.provided, instance_pool.provided, buffer.num * sizeof(unsigned));
+
+    free(buffer.buffer);
+    buffer = new_buffer;
+
+    instance_pool = new_pool;
+}
+
+void
+power_provider_component_manager::destroy_instance(instance i) {
+    auto last_index = buffer.num - 1;
+    auto last_entity = instance_pool.entity[last_index];
+    auto current_entity = instance_pool.entity[i.index];
+
+    instance_pool.entity[i.index] = instance_pool.entity[last_index];
+    instance_pool.max_provided[i.index] = instance_pool.max_provided[last_index];
+    instance_pool.provided[i.index] = instance_pool.provided[last_index];
+
+    entity_instance_map[last_entity] = i.index;
+    entity_instance_map.erase(current_entity);
+
+    --buffer.num;
+}
+
+void
+power_provider_component_manager::entity(c_entity const &e) {
+    if (buffer.num >= buffer.allocated) {
+        printf("Increasing size of power_provided_component buffer. Please adjust\n");
+        create_component_instance_data(std::max(1u, buffer.allocated) * 2);
+    }
+
+    auto inst = lookup(e);
+
+    instance_pool.entity[inst.index] = e;
+}
---
+++ src/component/power_provider_component.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "component_manager.h"
+
+// power component
+// max_provided -- max power able to be provided
+// unsigned
+// provided -- current power provided
+// unsigned
+
+struct power_provider_component_manager : component_manager {
+    struct power_instance_data {
+        c_entity *entity;
+        unsigned *max_provided;
+        unsigned *provided;
+    } instance_pool;
+
+    void create_component_instance_data(unsigned count) override;
+
+    void destroy_instance(instance i) override;
+
+    void entity(c_entity const &e) override;
+
+    unsigned & max_provided(c_entity const &e) {
+        auto inst = lookup(e);
+
+        return instance_pool.max_provided[inst.index];
+    }
+
+    unsigned & provided(c_entity const &e) {
+        auto inst = lookup(e);
+
+        return instance_pool.provided[inst.index];
+    }
+};
