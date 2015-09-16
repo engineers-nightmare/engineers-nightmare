c8c9f5cb3785313b1c533c0996bfa0b41936745f
Chris Forbes <chrisf@ijw.co.nz>
2015-09-16 21:03:36 +1200

    main: extract destroy_entity()
    I'm /sure/ i did this before. What happened?
    
    Signed-off-by: Chris Forbes <chrisf@ijw.co.nz>

--- main.cc
+++ main.cc
@@ -745,6 +745,111 @@ resize(int width, int height)
 
 
 void
+destroy_entity(entity *e)
+{
+    pos_man.destroy_entity_instance(e->ce);
+
+    render_man.destroy_entity_instance(e->ce);
+
+    teardown_static_physics_setup(nullptr, nullptr, &physics_man.rigid(e->ce));
+    physics_man.destroy_entity_instance(e->ce);
+
+    power_man.destroy_entity_instance(e->ce);
+
+    power_provider_man.destroy_entity_instance(e->ce);
+
+    gas_man.destroy_entity_instance(e->ce);
+
+    light_man.destroy_entity_instance(e->ce);
+
+    surface_man.destroy_entity_instance(e->ce);
+
+    switch_man.destroy_entity_instance(e->ce);
+
+    switchable_man.destroy_entity_instance(e->ce);
+
+    type_man.destroy_entity_instance(e->ce);
+
+    updateable_man.destroy_entity_instance(e->ce);
+
+    for (auto _type = 0; _type < num_wire_types; _type++) {
+        auto type = (wire_type)_type;
+        auto & entity_to_attach_lookup = ship->entity_to_attach_lookups[type];
+        auto & wire_attachments = ship->wire_attachments[type];
+
+        /* left side is the index of attach on entity that we're removing
+        * right side is the index we moved from the end into left side
+        * 0, 2 would be read as "attach at index 2 moved to index 0
+        * and assumed that what was at index 0 is no longer valid in referencers
+        */
+        std::unordered_map<unsigned, unsigned> fixup_attaches_removed;
+        auto entity_attaches = entity_to_attach_lookup.find(e->ce);
+        if (entity_attaches != entity_to_attach_lookup.end()) {
+            auto const & set = entity_attaches->second;
+            auto attaches = std::vector<unsigned>(set.begin(), set.end());
+            std::sort(attaches.begin(), attaches.end());
+
+            auto att_size = attaches.size();
+            for (size_t i = 0; i < att_size; ++i) {
+                auto attach = attaches[i];
+
+                // fill left side of fixup map
+                fixup_attaches_removed[attach] = 0;
+            }
+
+            /* Remove relevant attaches from wire_attachments
+            * relevant is an attach that isn't occupying a position
+            * will get popped off as a result of moving before removing
+            */
+            auto att_index = attaches.size() - 1;
+            auto swap_index = wire_attachments.size() - 1;
+            for (auto s = wire_attachments.rbegin();
+            s != wire_attachments.rend() && att_index != -1;) {
+
+                auto from_attach = wire_attachments[swap_index];
+                auto rem = attaches[att_index];
+                if (swap_index > rem) {
+                    wire_attachments[rem] = from_attach;
+                    wire_attachments.pop_back();
+                    fixup_attaches_removed[rem] = (unsigned)swap_index;
+                    --swap_index;
+                    ++s;
+                }
+                else if (s != wire_attachments.rend() && swap_index == rem) {
+                    wire_attachments.pop_back();
+                    fixup_attaches_removed.erase(rem);
+                    --swap_index;
+                    ++s;
+                }
+
+                --att_index;
+            }
+
+            /* remove all segments that contain an attach on entity */
+            for (auto remove_attach : attaches) {
+                remove_segments_containing(ship, type, remove_attach);
+            }
+
+            /* remove attaches assigned to entity from ship lookup */
+            entity_to_attach_lookup.erase(e->ce);
+
+            for (auto lookup : fixup_attaches_removed) {
+                /* we moved m to position r */
+                auto r = lookup.first;
+                auto m = lookup.second;
+
+                relocate_segments_and_entity_attaches(ship, type, r, m);
+            }
+
+            attach_topo_rebuild(ship, type);
+        }
+    }
+
+    delete e;
+}
+
+
+void
 remove_ents_from_surface(int x, int y, int z, int face)
 {
     chunk *ch = ship->get_chunk_containing(x, y, z);
@@ -763,105 +868,7 @@ remove_ents_from_surface(int x, int y, int z, int face)
         const auto & f = surface_man.face(e->ce);
 
         if (p.x == x && p.y == y && p.z == z && f == face) {
-            pos_man.destroy_entity_instance(e->ce);
-
-            render_man.destroy_entity_instance(e->ce);
-
-            teardown_static_physics_setup(nullptr, nullptr, &physics_man.rigid(e->ce));
-            physics_man.destroy_entity_instance(e->ce);
-
-            power_man.destroy_entity_instance(e->ce);
-            
-            power_provider_man.destroy_entity_instance(e->ce);
-
-            gas_man.destroy_entity_instance(e->ce);
-
-            light_man.destroy_entity_instance(e->ce);
-
-            surface_man.destroy_entity_instance(e->ce);
-
-            switch_man.destroy_entity_instance(e->ce);
-
-            switchable_man.destroy_entity_instance(e->ce);
-
-            type_man.destroy_entity_instance(e->ce);
-
-            updateable_man.destroy_entity_instance(e->ce);
-
-            for (auto _type = 0; _type < num_wire_types; _type++) {
-                auto type = (wire_type)_type;
-                auto & entity_to_attach_lookup = ship->entity_to_attach_lookups[type];
-                auto & wire_attachments = ship->wire_attachments[type];
-
-                /* left side is the index of attach on entity that we're removing
-                 * right side is the index we moved from the end into left side
-                 * 0, 2 would be read as "attach at index 2 moved to index 0
-                 * and assumed that what was at index 0 is no longer valid in referencers
-                 */
-                std::unordered_map<unsigned, unsigned> fixup_attaches_removed;
-                auto entity_attaches = entity_to_attach_lookup.find(e->ce);
-                if (entity_attaches != entity_to_attach_lookup.end()) {
-                    auto const & set = entity_attaches->second;
-                    auto attaches = std::vector<unsigned>(set.begin(), set.end());
-                    std::sort(attaches.begin(), attaches.end());
-
-                    auto att_size = attaches.size();
-                    for (size_t i = 0; i < att_size; ++i) {
-                        auto attach = attaches[i];
-
-                        // fill left side of fixup map
-                        fixup_attaches_removed[attach] = 0;
-                    }
-
-                    /* Remove relevant attaches from wire_attachments
-                     * relevant is an attach that isn't occupying a position
-                     * will get popped off as a result of moving before removing
-                     */
-                    auto att_index = attaches.size() - 1;
-                    auto swap_index = wire_attachments.size() - 1;
-                    for (auto s = wire_attachments.rbegin();
-                    s != wire_attachments.rend() && att_index != -1;) {
-
-                        auto from_attach = wire_attachments[swap_index];
-                        auto rem = attaches[att_index];
-                        if (swap_index > rem) {
-                            wire_attachments[rem] = from_attach;
-                            wire_attachments.pop_back();
-                            fixup_attaches_removed[rem] = (unsigned)swap_index;
-                            --swap_index;
-                            ++s;
-                        }
-                        else if (s != wire_attachments.rend() && swap_index == rem) {
-                            wire_attachments.pop_back();
-                            fixup_attaches_removed.erase(rem);
-                            --swap_index;
-                            ++s;
-                        }
-
-                        --att_index;
-                    }
-
-                    /* remove all segments that contain an attach on entity */
-                    for (auto remove_attach : attaches) {
-                        remove_segments_containing(ship, type, remove_attach);
-                    }
-
-                    /* remove attaches assigned to entity from ship lookup */
-                    entity_to_attach_lookup.erase(e->ce);
-
-                    for (auto lookup : fixup_attaches_removed) {
-                        /* we moved m to position r */
-                        auto r = lookup.first;
-                        auto m = lookup.second;
-
-                        relocate_segments_and_entity_attaches(ship, type, r, m);
-                    }
-
-                    attach_topo_rebuild(ship, type);
-                }
-            }
-
-            delete e;
+            destroy_entity(e);
             it = ch->entities.erase(it);
         }
         else {
