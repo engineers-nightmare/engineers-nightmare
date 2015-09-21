289e2f13e77864aca5db730df44810b4e7ee71c4
Caleb Anderson <robotrising@gmail.com>
2015-09-21 11:07:22 -0600

    components: add pressure sensor component

--- src/component/component_system_manager.cc
+++ src/component/component_system_manager.cc
@@ -6,6 +6,7 @@ physics_component_manager physics_man;
 relative_position_component_manager pos_man;
 power_component_manager power_man;
 power_provider_component_manager power_provider_man;
+pressure_sensor_component_manager pressure_man;
 renderable_component_manager render_man;
 surface_attachment_component_manager surface_man;
 switch_component_manager switch_man;
---
+++ src/component/pressure_sensor_component.cc
@@ -0,0 +1,59 @@
+#include <algorithm>
+#include "../memory.h"
+#include "pressure_sensor_component.h"
+
+void
+pressure_sensor_component_manager::create_component_instance_data(unsigned count) {
+    if (count <= buffer.allocated)
+        return;
+
+    component_buffer new_buffer;
+    instance_data new_pool;
+
+    size_t size = sizeof(c_entity) * count;
+    size = sizeof(float) * count + align_size<float>(size);
+    size += alignof(c_entity);  // for worst-case misalignment of initial ptr
+
+    new_buffer.buffer = malloc(size);
+    new_buffer.num = buffer.num;
+    new_buffer.allocated = count;
+    memset(new_buffer.buffer, 0, size);
+
+    new_pool.entity = align_ptr((c_entity *)new_buffer.buffer);
+    new_pool.pressure = align_ptr((float *)(new_pool.entity + count));
+
+    memcpy(new_pool.entity, instance_pool.entity, buffer.num * sizeof(c_entity));
+    memcpy(new_pool.pressure, instance_pool.pressure, buffer.num * sizeof(float));
+
+    free(buffer.buffer);
+    buffer = new_buffer;
+
+    instance_pool = new_pool;
+}
+
+void
+pressure_sensor_component_manager::destroy_instance(instance i) {
+    auto last_index = buffer.num - 1;
+    auto last_entity = instance_pool.entity[last_index];
+    auto current_entity = instance_pool.entity[i.index];
+
+    instance_pool.entity[i.index] = instance_pool.entity[last_index];
+    instance_pool.pressure[i.index] = instance_pool.pressure[last_index];
+
+    entity_instance_map[last_entity] = i.index;
+    entity_instance_map.erase(current_entity);
+
+    --buffer.num;
+}
+
+void
+pressure_sensor_component_manager::entity(const c_entity& e) {
+    if (buffer.num >= buffer.allocated) {
+        printf("Increasing size of switch buffer. Please adjust\n");
+        create_component_instance_data(std::max(1u, buffer.allocated) * 2);
+    }
+
+    auto inst = lookup(e);
+
+    instance_pool.entity[inst.index] = e;
+}
