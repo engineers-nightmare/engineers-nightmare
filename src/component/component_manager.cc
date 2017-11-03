25ff9965be35bffb41f72fe7536241dc22318e7f
Caleb Anderson <robotrising@gmail.com>
2017-11-02 20:32:52 -0600

    comp: shared_ptr

--- src/component/component_manager.cc
+++ src/component/component_manager.cc
@@ -1,6 +1,8 @@
 //
 // Created by caleb on 11/1/17.
 //
+#include <memory>
+
 #include "component_manager.h"
 
-std::unordered_map<std::string, std::function<component_stub(config_setting_t *)>> component_stub_generators;
+std::unordered_map<std::string, std::function<std::shared_ptr<component_stub>(config_setting_t *)>> component_stub_generators;
