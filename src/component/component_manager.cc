6f2b867bfa11423772135fc9448ed9cc96b8177c
Caleb Anderson <robotrising@gmail.com>
2017-11-05 02:05:13 -0700

    build: remove old file

--- src/component/component_manager.cc
+++
@@ -1,8 +0,0 @@
-//
-// Created by caleb on 11/1/17.
-//
-#include <memory>
-
-#include "component_manager.h"
-
-std::unordered_map<std::string, std::function<std::unique_ptr<component_stub>(config_setting_t *)>> component_stub_generators;
