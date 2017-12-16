5dab8521f9bfec0d62ddce75e2f1412611afc1cc
Caleb Anderson <robotrising@gmail.com>
2017-12-16 01:37:32 -0700

    tool: remove entity looking at instead of all on surface

--- src/tools/remove_entity.cc
+++ src/tools/remove_entity.cc
@@ -5,6 +5,7 @@
 #include "../ship_space.h"
 #include "../mesh.h"
 #include "tools.h"
+#include "../component/component_system_manager.h"
 
 
 extern GLuint overlay_shader;
@@ -12,20 +13,27 @@ extern GLuint simple_shader;
 
 extern ship_space *ship;
 
+extern player pl;
+
 extern asset_manager asset_man;
+
+extern void destroy_entity(c_entity e);
+
+extern component_system_manager component_system_man;
 
 struct remove_entity_tool : tool
 {
+    bool dirty = false;
+
     bool can_use(raycast_info *rc) {
-        return rc->block.hit;
+        return rc->world.hit && c_entity::is_valid(rc->world.entity);
     }
 
     void use(raycast_info *rc) override {
         if (!can_use(rc))
             return;
 
-        int index = normal_to_surface_index(&rc->block);
-        remove_ents_from_surface(rc->block.p, index ^ 1);
+        destroy_entity(rc->world.entity);
     }
 
     void alt_use(raycast_info *rc) override {}
@@ -35,34 +43,22 @@ struct remove_entity_tool : tool
     void cycle_mode() override {}
 
     void preview(raycast_info *rc, frame_data *frame) override {
-        if (!can_use(rc))
-            return;
-
-        int index = normal_to_surface_index(&rc->block);
-        block *other_side = ship->get_block(rc->block.p);
-
-        if (!other_side) {
-            return;
+        auto use = can_use(rc);
+        if (use != dirty) {
+            pl.ui_dirty = true;
+            use = !use;
         }
-
-        auto material = asset_man.get_world_texture_index("white");
-
-        auto mat = frame->alloc_aligned<mesh_instance>(1);
-        mat.ptr->world_matrix = mat_position(glm::vec3(rc->block.bl));
-        mat.ptr->material = material;
-        mat.bind(1, frame);
-
-        auto surf_mesh = asset_man.get_surface_mesh(index, surface_wall);
-
-        glUseProgram(overlay_shader);
-        glEnable(GL_POLYGON_OFFSET_FILL);
-        draw_mesh(surf_mesh.hw);
-        glDisable(GL_POLYGON_OFFSET_FILL);
-        glUseProgram(simple_shader);
     }
 
     void get_description(raycast_info *rc, char *str) override {
-        strcpy(str, "Remove entity tool");
+        if (rc->world.hit && c_entity::is_valid(rc->world.entity)) {
+            auto &type_man = component_system_man.managers.type_component_man;
+            auto type = type_man.get_instance_data(rc->world.entity);
+            sprintf(str, "Remove %s", *type.name);
+        }
+        else {
+            strcpy(str, "Remove entity tool");
+        }
     }
 };
 
