4c4d5616c35da37669f2c4a26979abfd3a5a1cb3
Caleb Anderson <robotrising@gmail.com>
2018-03-11 11:22:24 -0600

    extract draw fp mesh

--- src/tools/customize_entity.cc
+++ src/tools/customize_entity.cc
@@ -54,22 +54,7 @@ struct customize_entity_tool : tool
         auto &rend = component_system_man.managers.renderable_component_man;
         auto &pos_man = component_system_man.managers.position_component_man;
 
-        auto fp_mesh = &asset_man.get_mesh("fp_customize_tool");
-
-
-        glm::vec3 fp_item_offset{ 0.115f, 0.2f, -0.12f };
-        float fp_item_scale{ 0.175f };
-        glm::quat fp_item_rot{ -5.f, 5.f, 5.f, 5.f };
-
-        auto m = glm::mat4_cast(glm::normalize(pl.rot));
-        auto right = glm::vec3(m[0]);
-        auto up = glm::vec3(m[1]);
-
-        auto fpmat = frame->alloc_aligned<mesh_instance>(1);
-        fpmat.ptr->world_matrix = glm::scale(mat_position(pl.eye + pl.dir * fp_item_offset.y + right * fp_item_offset.x + up * fp_item_offset.z), glm::vec3(fp_item_scale)) * m * glm::mat4_cast(glm::normalize(fp_item_rot));
-        fpmat.ptr->color = glm::vec4(1.f, 1.f, 1.f, 1.f);
-        fpmat.bind(1, frame);
-        draw_mesh(fp_mesh->hw);
+        draw_fp_mesh(frame);
 
         if (entity != rc.entity) {
             if (c_entity::is_valid(entity) && rend.exists(entity)) {
@@ -98,6 +83,25 @@ struct customize_entity_tool : tool
             glDisable(GL_BLEND);
             glUseProgram(simple_shader);
         }
+    }
+
+    void draw_fp_mesh(frame_data *frame) const {
+        auto fp_mesh = &asset_man.get_mesh("fp_customize_tool");
+
+        glm::vec3 fp_item_offset{0.115f, 0.2f, -0.115f };
+        float fp_item_scale{ 0.175f };
+        glm::quat fp_item_rot{-5.f, 5.f, 5.f, 5.f };
+
+        auto m = mat4_cast(normalize(pl.rot));
+        auto right = glm::vec3(m[0]);
+        auto up = glm::vec3(m[1]);
+
+        auto fpmat = frame->alloc_aligned<mesh_instance>(1);
+        fpmat.ptr->world_matrix = scale(mat_position(pl.eye + pl.dir * fp_item_offset.y + right * fp_item_offset.x + up * fp_item_offset.z), glm::vec3(fp_item_scale)) * m * mat4_cast(
+                normalize(fp_item_rot));
+        fpmat.ptr->color = glm::vec4(1.f, 1.f, 1.f, 1.f);
+        fpmat.bind(1, frame);
+        draw_mesh(fp_mesh->hw);
     }
 
     void get_description(char *str) override {
