b5c9aedd235ae72b29bbd27d0004e0793f61ce39
Caleb Anderson <robotrising@gmail.com>
2018-03-11 13:21:20 -0600

    fumbling

--- src/tools/customize_entity.cc
+++ src/tools/customize_entity.cc
@@ -7,9 +7,12 @@
 #include "../player.h"
 #include "tools.h"
 #include "../component/component_system_manager.h"
+#include "../imgui/imgui.h"
+#include "../imgui_impl_sdl_gl3.h"
 
 extern GLuint simple_shader;
 extern GLuint highlight_shader;
+extern GLuint modelspace_uv_shader;
 
 extern ship_space *ship;
 
@@ -23,23 +26,27 @@ extern void destroy_entity(c_entity e);
 
 extern component_system_manager component_system_man;
 
+extern std::array<ImGuiContext*, 3> offscreen_contexts;
+extern GLuint render_displays_fbo;
+extern void new_imgui_frame();
+
+const glm::vec3 fp_item_offset{0.115f, 0.2f, -0.115f };
+const float fp_item_scale{ 0.175f };
+const glm::quat fp_item_rot{-5.f, 5.f, 5.f, 5.f };
+
 struct customize_entity_tool : tool
 {
     c_entity entity;
     raycast_info_world rc;
 
     void pre_use(player *pl) {
+        // todo: cast from tool instead of eye?
         phys_raycast_world(pl->eye, pl->eye + 2.f * pl->dir,
                            phy->rb_controller.get(), phy->dynamicsWorld.get(), &rc);
     }
 
     bool can_use() {
-        if (!rc.hit || !c_entity::is_valid(entity)) {
-            return false;
-        }
-
-        auto &sam = component_system_man.managers.surface_attachment_component_man;
-        return sam.exists(entity) && *sam.get_instance_data(entity).attached;
+        return !(!rc.hit || !c_entity::is_valid(entity));
     }
 
     void use() override {
@@ -53,8 +60,8 @@ struct customize_entity_tool : tool
     void preview(frame_data *frame) override {
         auto &rend = component_system_man.managers.renderable_component_man;
         auto &pos_man = component_system_man.managers.position_component_man;
-
-        draw_fp_mesh(frame);
+        auto &type_man = component_system_man.managers.type_component_man;
+        auto &wire_man = component_system_man.managers.wire_comms_component_man;
 
         if (entity != rc.entity) {
             if (c_entity::is_valid(entity) && rend.exists(entity)) {
@@ -83,15 +90,48 @@ struct customize_entity_tool : tool
             glDisable(GL_BLEND);
             glUseProgram(simple_shader);
         }
+
+        auto fp_mesh = &asset_man.get_mesh("fp_customize_tool");
+        draw_fp_mesh(frame, fp_mesh);
+
+        asset_man.bind_render_textures(0);
+
+        ImGui::SetCurrentContext(offscreen_contexts[2]);
+        new_imgui_frame();
+        ImGui::GetIO().DisplaySize = ImVec2(RENDER_DIM, RENDER_DIM);
+        ImGui::GetIO().DisplayFramebufferScale = ImVec2(1, 1);
+
+        auto flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize |
+                     ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoScrollbar |
+                     ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoInputs;
+        // center on screen
+        // todo: modify NewFrame() to allow us to use this properly
+        ImGui::SetNextWindowPos(ImVec2{ RENDER_DIM / 2, RENDER_DIM / 4}, 0, ImVec2{ 0.5f, 0.5f });
+        {
+            ImGui::Begin("First Window", nullptr, flags);
+            {
+                static float f = 0.0f;
+                ImGui::Text("Hello, world!");
+                ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+                ImGui::Button("Test Window");
+                ImGui::Button("Another Window");
+                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            }
+            ImGui::End();
+            glBindFramebuffer(GL_DRAW_FRAMEBUFFER, render_displays_fbo);
+            glFramebufferTextureLayer(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, asset_man.render_textures->texobj, 0, 2);
+            glViewport(0, 0, RENDER_DIM, RENDER_DIM);
+            glClearColor(0, 0.18f, 0.21f, 1);
+            glClear(GL_COLOR_BUFFER_BIT);
+            ImGui::Render();
+        }
+
+        auto fp_screen = &asset_man.get_mesh("fp_customize_tool_screen");
+        glUseProgram(modelspace_uv_shader);
+        draw_fp_display_mesh(frame, fp_screen, 2);
     }
 
-    void draw_fp_mesh(frame_data *frame) const {
-        auto fp_mesh = &asset_man.get_mesh("fp_customize_tool");
-
-        glm::vec3 fp_item_offset{0.115f, 0.2f, -0.115f };
-        float fp_item_scale{ 0.175f };
-        glm::quat fp_item_rot{-5.f, 5.f, 5.f, 5.f };
-
+    void draw_fp_mesh(frame_data *frame, const mesh_data *mesh) const {
         auto m = mat4_cast(normalize(pl.rot));
         auto right = glm::vec3(m[0]);
         auto up = glm::vec3(m[1]);
@@ -101,7 +141,20 @@ struct customize_entity_tool : tool
                 normalize(fp_item_rot));
         fpmat.ptr->color = glm::vec4(1.f, 1.f, 1.f, 1.f);
         fpmat.bind(1, frame);
-        draw_mesh(fp_mesh->hw);
+        draw_mesh(mesh->hw);
+    }
+
+    void draw_fp_display_mesh(frame_data *frame, const mesh_data *mesh, int layer) const {
+        auto m = mat4_cast(normalize(pl.rot));
+        auto right = glm::vec3(m[0]);
+        auto up = glm::vec3(m[1]);
+
+        auto fpmat = frame->alloc_aligned<display_mesh_instance>(1);
+        fpmat.ptr->world_matrix = scale(mat_position(pl.eye + pl.dir * fp_item_offset.y + right * fp_item_offset.x + up * fp_item_offset.z), glm::vec3(fp_item_scale)) * m * mat4_cast(
+                normalize(fp_item_rot));
+        fpmat.ptr->material = layer;
+        fpmat.bind(1, frame);
+        draw_mesh(mesh->hw);
     }
 
     void get_description(char *str) override {
