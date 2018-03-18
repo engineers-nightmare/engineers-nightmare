ad2094bf7d425473f1b2dadd966d5e4f16582e2c
Caleb Anderson <robotrising@gmail.com>
2018-03-17 18:02:43 -0600

    modify wiring label with cusotmize tool

--- src/game_state/customize_entity_state.h
+++ src/game_state/customize_entity_state.h
@@ -33,11 +33,8 @@ extern glm::quat fp_item_rot;
 
 struct customize_entity_state : game_state {
     enum class MenuState {
-        Main,
-        Game,
-        Settings,
-        Keybinds,
-    } state{MenuState::Main};
+        Comms,
+    } state{MenuState::Comms};
 
     c_entity entity;
 
@@ -47,19 +44,14 @@ struct customize_entity_state : game_state {
 
     explicit customize_entity_state(c_entity e) : entity(e) {
         warp_mouse_to_center_screen();
-        state = MenuState::Main;
+        state = MenuState::Comms;
     }
 
     void handle_input() override {
         if (get_input(action_menu)->just_active) {
             switch (state) {
-                case MenuState::Main:
+                case MenuState::Comms:
                     set_next_game_state(create_play_state());
-                    break;
-                case MenuState::Game:
-                case MenuState::Settings:
-                case MenuState::Keybinds:
-                    state = MenuState::Main;
                     break;
             }
         }
@@ -74,115 +66,39 @@ struct customize_entity_state : game_state {
         }
     }
 
-    void handle_main_menu() {
-        // todo: use a proper state transition system
-        if (settings_dirty) {
-            save_settings(game_settings);
-            apply_video_settings();
-            settings_dirty = false;
-        }
+    void handle_comms_menu() {
+        auto &type_man = component_system_man.managers.type_component_man;
+        auto &wire_man = component_system_man.managers.wire_comms_component_man;
 
-        ImGui::Begin("", nullptr, menu_flags);
+        auto type = type_man.get_instance_data(entity);
+        auto wire = wire_man.get_instance_data(entity);
+
+        ImGui::Begin("Comms Configuration", nullptr, menu_flags);
         {
-            ImGui::Text("Engineer's Nightmare");
-            ImGui::Separator();
-            ImGui::Dummy(ImVec2{ 10, 10 });
-            if (ImGui::Button("Resume Game")) {
-                set_next_game_state(create_play_state());
+            if (!c_entity::is_valid(entity)) {
+                ImGui::Text("Invalid Entity %d", entity.id);
             }
-            ImGui::Dummy(ImVec2{10, 10});
-            if (ImGui::Button("Game")) {
-                state = MenuState::Game;
-            }
-            ImGui::Dummy(ImVec2{10, 10});
-            if (ImGui::Button("Settings")) {
-                state = MenuState::Settings;
-            }
-            ImGui::Dummy(ImVec2{10, 10});
-            if (ImGui::Button("Exit Game")) {
-                request_exit();
-            }
-        }
-        ImGui::End();
-    }
-    void handle_game_menu() {
-        ImGui::Begin("", nullptr, menu_flags);
-        {
-            ImGui::Text("Game");
-            ImGui::Separator();
-            ImGui::Dummy(ImVec2{10, 10});
+            else {
+                auto *label = new char[256];
 
-            if (ImGui::Button("Save")) {
-                // we may want to get clever and pause the world or defer this to
-                // a particular time
-                save(ship, "save/ship.out");
+                if (*wire.label != nullptr) {
+                    strcpy(label, *wire.label);
+                }
+                else {
+                    strcpy(label, "");
+                }
 
-                set_next_game_state(create_play_state());
-            }
-            ImGui::Dummy(ImVec2{10, 10});
-            if (ImGui::Button("Load")) {
-                // we may want to get clever and pause the world or defer this to
-                // a particular time
-                teardown_chunks();
-                delete ship;
-                ship = new ship_space();
-                load(ship, "save/ship.out");
+                ImGui::Text("%s", *type.name);
+                ImGui::Separator();
+                ImGui::Dummy(ImVec2{10, 10});
+                ImGui::InputText("Label", label, 256, ImGuiInputTextFlags_AutoSelectAll);
+                ImGui::Dummy(ImVec2{10, 10});
+                if (ImGui::Button("Finish")) {
+                    set_next_game_state(create_play_state());
+                }
 
-                set_next_game_state(create_play_state());
-            }
-        }
-        ImGui::End();
-    }
-
-    void handle_settings_menu() {
-        ImGui::Begin("", nullptr, menu_flags);
-        {
-            ImGui::Text("Settings");
-            ImGui::Separator();
-
-            std::array<const char*, 2> modes{
-                    get_enum_description(window_mode::windowed),
-                    get_enum_description(window_mode::fullscreen),
-            };
-            auto mode = game_settings.video.mode;
-            settings_dirty |= ImGui::Combo("Window Mode", (int*)&mode, modes.data(), (int)modes.size());
-            game_settings.video.mode = mode;
-
-            auto fov = glm::radians(game_settings.video.fov);
-            settings_dirty |= ImGui::SliderAngle("Field of View", &fov, 50.0f, 120.0f);
-            game_settings.video.fov = glm::degrees(fov);
-
-            bool invert = game_settings.input.mouse_invert == -1.0f;
-            settings_dirty |= ImGui::Checkbox("Invert Mouse", &invert);
-            game_settings.input.mouse_invert = invert ? -1.0f : 1.0f;
-
-            float vol = audio->getGlobalVolume();
-            settings_dirty |= ImGui::SliderFloat("Volume", &vol, 0.0f, 1.0f);
-            game_settings.audio.global_volume = vol;
-            audio->setGlobalVolume(vol);
-
-            ImGui::Separator();
-            ImGui::Checkbox("Draw FPS", &draw_fps);
-            ImGui::Separator();
-
-            // debug
-            bool draw_debug = draw_debug_text && draw_debug_chunks && draw_debug_axis && draw_debug_physics;
-            if (ImGui::Checkbox("Draw Debug All", &draw_debug)) {
-                draw_debug_text = draw_debug_chunks = draw_debug_axis = draw_debug_physics = draw_debug;
-            }
-            ImGui::Checkbox("Draw Debug Text", &draw_debug_text);
-            ImGui::Checkbox("Draw Chunk Debug", &draw_debug_chunks);
-            ImGui::Checkbox("Draw Axis Debug", &draw_debug_axis);
-            ImGui::Checkbox("Draw Physics Debug", &draw_debug_physics);
-
-            ImGui::Separator();
-            ImGui::SliderFloat3("FP item offset", glm::value_ptr(fp_item_offset), -0.5f, 0.5f);
-            ImGui::SliderFloat("FP item scale", &fp_item_scale, 0.f, 1.f);
-            ImGui::SliderFloat4("FP item rot", glm::value_ptr(fp_item_rot), -5.f, 5.f);
-
-            ImGui::Dummy(ImVec2{ 10, 10 });
-            if (ImGui::Button("Back")) {
-                state = MenuState::Main;
+                delete[] *wire.label;
+                *wire.label = label;
             }
         }
         ImGui::End();
@@ -198,20 +114,8 @@ struct customize_entity_state : game_state {
         ImGui::SetNextWindowPos(ImVec2{ io.DisplaySize.x / 2.0f, io.DisplaySize.y / 2.0f }, 0, ImVec2{ 0.5f, 0.5f });
         {
             switch (state) {
-                case MenuState::Main: {
-                    handle_main_menu();
-                    break;
-                }
-                case MenuState::Game: {
-                    handle_game_menu();
-                    break;
-                }
-                case MenuState::Settings: {
-                    handle_settings_menu();
-                    break;
-                }
-                case MenuState::Keybinds: {
-//                    handle_keybinds_menu();
+                case MenuState::Comms: {
+                    handle_comms_menu();
                     break;
                 }
             }
