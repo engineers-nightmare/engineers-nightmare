137e6527a5ceb306768a8798eaee6457d324d44d
Caleb Anderson <robotrising@gmail.com>
2018-03-28 20:07:50 -0600

    first pass on filters in customize menu. crashes at render after input to filters. :?
    read from genned ui bits
    
    save to filter on component

--- src/game_state/customize_entity_state.cc
+++ src/game_state/customize_entity_state.cc
@@ -1,5 +1,6 @@
 #include <SDL_mouse.h>
 #include <soloud.h>
+#include <map>
 
 #include "../game_state.h"
 #include "../imgui/imgui.h"
@@ -18,6 +19,8 @@ extern void apply_video_settings();
 extern void request_exit();
 extern void new_imgui_frame();
 extern void teardown_chunks();
+extern std::map<const std::string, std::string> get_filter_map(c_entity);
+extern void update_filter(c_entity entity, std::string const&, char[256]);
 
 extern en_settings game_settings;
 extern ship_space *ship;
@@ -33,32 +36,32 @@ extern float fp_item_scale;
 extern glm::quat fp_item_rot;
 
 struct customize_entity_state : game_state {
-    enum class MenuState {
-        Comms,
-    } state{MenuState::Comms};
+    enum class CustomizeState {
+        Main,
+        CommsOutput,
+        CommsFilter,
+    } state{CustomizeState::Main}, next_state{state};
 
     c_entity entity;
 
-    bool settings_dirty = false;
-    char entity_label[256];
+    char entity_label[256]{};
+    std::map<const std::string, std::string> comp_name_to_filter_name;
 
     unsigned menu_flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_AlwaysAutoResize;
 
     explicit customize_entity_state(c_entity e) : entity(e) {
         warp_mouse_to_center_screen();
-        state = MenuState::Comms;
-
-        if (c_entity::is_valid(entity)) {
-            auto &wire_man = component_system_man.managers.wire_comms_component_man;
-            auto wire = wire_man.get_instance_data(entity);
-            strcpy(entity_label, (*wire.label) ? *wire.label : "");
-        }
+        state = CustomizeState::Main;
     }
 
     void handle_input() override {
         if (get_input(action_menu)->just_active) {
             switch (state) {
-                case MenuState::Comms:
+                case CustomizeState::CommsOutput:
+                case CustomizeState::CommsFilter:
+                    set_next_state(CustomizeState::Main);
+                    break;
+                case CustomizeState::Main:
                     set_next_game_state(create_play_state());
                     break;
             }
@@ -68,14 +71,40 @@ struct customize_entity_state : game_state {
     void rebuild_ui() override {
     }
 
+    void switch_state(CustomizeState new_state) {
+        switch (new_state) {
+            case CustomizeState::CommsOutput: {
+                auto &wire_man = component_system_man.managers.wire_comms_component_man;
+                auto wire = wire_man.get_instance_data(entity);
+                strcpy(entity_label, (*wire.label) ? *wire.label : "");
+                break;
+            }
+            case CustomizeState::CommsFilter: {
+                comp_name_to_filter_name = get_filter_map(entity);
+                break;
+            }
+            default:
+                break;
+        }
+        state = new_state;
+    }
+
+    void set_next_state(CustomizeState new_state) {
+        next_state = new_state;
+    }
+
     void update(float dt) override {
         if (window_has_focus() && SDL_GetRelativeMouseMode() == SDL_TRUE) {
             SDL_SetRelativeMouseMode(SDL_FALSE);
         }
+
+        if (next_state != state) {
+            switch_state(next_state);
+        }
     }
 
-    void handle_comms_menu() {
-        ImGui::Begin("Comms Configuration", nullptr, menu_flags);
+    void handle_main_state() {
+        ImGui::Begin("Main", nullptr, menu_flags);
         {
             if (!c_entity::is_valid(entity)) {
                 ImGui::Text("Invalid Entity %d", entity.id);
@@ -87,17 +116,89 @@ struct customize_entity_state : game_state {
                 ImGui::Text("%s", *type.name);
                 ImGui::Separator();
                 ImGui::Dummy(ImVec2{10, 10});
-                bool isDone = ImGui::InputText("Label", entity_label, 256, ImGuiInputTextFlags_AutoSelectAll | ImGuiInputTextFlags_EnterReturnsTrue);
+                if (ImGui::Button("Output")) {
+                    set_next_state(CustomizeState::CommsOutput);
+                }
+                else if (ImGui::Button("Filters")) {
+                    set_next_state(CustomizeState::CommsFilter);
+                }
+                else if (ImGui::Button("Back")) {
+                    set_next_game_state(create_play_state());
+                }
+            }
+        }
+        ImGui::End();
+    }
+
+    void handle_comms_output_state() {
+        ImGui::Begin("Comms Output", nullptr, menu_flags);
+        {
+            if (!c_entity::is_valid(entity)) {
+                ImGui::Text("Invalid Entity %d", entity.id);
+            }
+            else {
+                auto &type_man = component_system_man.managers.type_component_man;
+                auto type = type_man.get_instance_data(entity);
+
+                if (ImGui::IsWindowAppearing()) {
+                    ImGui::SetKeyboardFocusHere(0);
+                }
+                ImGui::Text("%s", *type.name);
+                ImGui::Separator();
                 ImGui::Dummy(ImVec2{10, 10});
-                isDone = ImGui::Button("Finish") || isDone;
-                if (isDone) {
+                bool save_label = ImGui::InputText("Comms Label", entity_label, 256, ImGuiInputTextFlags_AutoSelectAll | ImGuiInputTextFlags_EnterReturnsTrue);
+                ImGui::Dummy(ImVec2{10, 10});
+                save_label = ImGui::Button("Save") || save_label;
+                if (save_label) {
                     auto &wire_man = component_system_man.managers.wire_comms_component_man;
                     auto wire = wire_man.get_instance_data(entity);
 
                     free((void *) *wire.label);
                     *wire.label = strdup(entity_label);
+                }
 
-                    set_next_game_state(create_play_state());
+                if (ImGui::Button("Back")) {
+                    set_next_state(CustomizeState::Main);
+                }
+            }
+        }
+        ImGui::End();
+    }
+
+    void handle_comms_filter_state() {
+        ImGui::Begin("Comms Filter", nullptr, menu_flags);
+        {
+            if (!c_entity::is_valid(entity)) {
+                ImGui::Text("Invalid Entity %d", entity.id);
+            }
+            else {
+                auto &type_man = component_system_man.managers.type_component_man;
+                auto type = type_man.get_instance_data(entity);
+
+                if (ImGui::IsWindowAppearing()) {
+                    ImGui::SetKeyboardFocusHere(0);
+                }
+                ImGui::Text("%s", *type.name);
+                ImGui::Separator();
+                ImGui::Dummy(ImVec2{10, 10});
+                for (auto && kvp: comp_name_to_filter_name) {
+                    auto comp = kvp.first;
+                    char filter[256];
+                    strcpy(filter, kvp.second.c_str());
+
+                    bool save_filter = ImGui::InputText(comp.c_str(), filter, 256,
+                                                       ImGuiInputTextFlags_AutoSelectAll |
+                                                       ImGuiInputTextFlags_EnterReturnsTrue);
+                    comp_name_to_filter_name[comp] = filter;
+                    ImGui::Dummy(ImVec2{10, 10});
+                    save_filter = ImGui::Button("Save") || save_filter;
+                    if (save_filter) {
+                        update_filter(entity, comp, filter);
+                    }
+                }
+
+                if (ImGui::Button("Back")) {
+                    set_next_state(CustomizeState::Main);
                 }
             }
         }
@@ -114,10 +215,15 @@ struct customize_entity_state : game_state {
         ImGui::SetNextWindowPos(ImVec2{ io.DisplaySize.x / 2.0f, io.DisplaySize.y / 2.0f }, 0, ImVec2{ 0.5f, 0.5f });
         {
             switch (state) {
-                case MenuState::Comms: {
-                    handle_comms_menu();
+                case CustomizeState::Main:
+                    handle_main_state();
                     break;
-                }
+                case CustomizeState::CommsOutput:
+                    handle_comms_output_state();
+                    break;
+                case CustomizeState::CommsFilter:
+                    handle_comms_filter_state();
+                    break;
             }
         }
 
