0dad98720de83077c67f25616fd928d08c03995c
Caleb Anderson <robotrising@gmail.com>
2018-04-06 16:18:48 -0600

    update gen to load components up front. regen, but result in no new data (a good thing)

--- gen/gen_comps.py
+++ gen/gen_comps.py
@@ -12,9 +12,44 @@ def find_matching_stub(body_field, stubs):
     return None
 
 
+class component:
+    def __init__(self, cname, cfile, sfields, bfields, depends):
+        self.comp_name = cname
+        self.comp_file = cfile
+        self.stub_fields = sfields
+        self.body_fields = bfields
+        self.dependencies = depends
+
+
+def load_component(comp_file):
+    stub_fields = []
+    body_fields = []
+    dependencies = []
+    component_name = os.path.basename(comp_file)
+    with open(comp_file, 'r') as f:
+        prev = 'entity'
+        for l in f:
+            parts = l.strip().split(',')
+            if parts[0] == 'body':
+                body_fields.append({'type': parts[1], 'name': parts[2], 'default': parts[3], 'prev': prev,
+                                    'comp_name': component_name})
+                prev = parts[2]
+            elif parts[0] == 'stub':
+                stub_fields.append(
+                    {'otype': parts[1], 'type': parts[2], 'pre': parts[3], 'name': parts[4], 'extra': parts[5],
+                     'comp_name': component_name})
+            elif parts[0] == 'depends':
+                dependencies = parts[1:]
+            for body in body_fields:
+                body['stub'] = find_matching_stub(body, stub_fields)
+
+    comp = component(component_name, comp_file, stub_fields, body_fields, dependencies)
+    return comp
+
+
 def main():
     files = [f for f in glob.glob('gen/comp/*')]
-    comp_names = sorted([os.path.basename(f) for f in files])
+    comps = {f.split('/')[2]: load_component(f) for f in files}
 
     component_managers_header_template = Template(filename='gen/templates/component_managers.h.gen')
     component_header_template = Template(filename='gen/templates/comp_component.h.gen')
@@ -22,39 +57,19 @@ def main():
 
     print("Genning header: src/component/component_managers.h")
     with open("src/component/component_managers.h", "w") as g:
-        g.write(component_managers_header_template.render(comp_names=comp_names))
+        g.write(component_managers_header_template.render(comps=sorted(comps)))
 
-    for comp_file in files:
-        print("Genning from %s to " % comp_file)
-        stub_fields = []
-        body_fields = []
-        dependencies = []
-        component_name = os.path.basename(comp_file)
-        with open(comp_file, 'r') as f:
-            prev = 'entity'
-            for l in f:
-                parts = l.strip().split(',')
-                if parts[0] == 'body':
-                    body_fields.append({'type': parts[1], 'name': parts[2], 'default': parts[3], 'prev': prev,
-                                        'comp_name': component_name})
-                    prev = parts[2]
-                elif parts[0] == 'stub':
-                    stub_fields.append(
-                        {'otype': parts[1], 'type': parts[2], 'pre': parts[3], 'name': parts[4], 'extra': parts[5],
-                         'comp_name': component_name})
-                elif parts[0] == 'depends':
-                    dependencies = parts[1:]
-                for body in body_fields:
-                    body['stub'] = find_matching_stub(body, stub_fields)
-        opts = {'comp_name': component_name, 'stubs': stub_fields, 'bodies': body_fields, 'dependencies': dependencies}
+    for comp_name in sorted(comps.keys()):
+        print("Genning from %s to " % comp_name)
+        comp = comps[comp_name]
 
-        print("  header: src/component/%s_component.h" % component_name)
-        with open("src/component/%s_component.h" % component_name, "w") as g:
-            g.write(component_header_template.render(**opts))
+        print("  header: src/component/%s_component.h" % comp_name)
+        with open("src/component/%s_component.h" % comp_name, "w") as g:
+            g.write(component_header_template.render(comp_name=comp_name, comp=comp))
 
-        print("  source: src/component/%s_component.cc" % component_name)
-        with open("src/component/%s_component.cc" % component_name, "w") as g:
-            g.write(component_impl_template.render(**opts))
+        print("  source: src/component/%s_component.cc" % comp_name)
+        with open("src/component/%s_component.cc" % comp_name, "w") as g:
+            g.write(component_impl_template.render(comp_name=comp_name, comp=comp))
 
     return 0
 
