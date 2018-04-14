b0855c2008655b0c5b0e14b81a9abd792da2d08c
Caleb Anderson <robotrising@gmail.com>
2018-04-14 12:39:51 -0600

    break customize_entity_state up into two files

---
+++ src/game_state/customize_entity_comms_output_state.cc
@@ -0,0 +1,95 @@
+#include <SDL_mouse.h>
+#include <vector>
+
+#include "../game_state.h"
+#include "../imgui/imgui.h"
+#include "../input.h"
+#include "../component/component_system_manager.h"
+
+extern action const* get_input(en_action a);
+extern void set_next_game_state(game_state *s);
+
+extern void warp_mouse_to_center_screen();
+extern bool window_has_focus();
+extern void new_imgui_frame();
+extern std::vector<std::pair<const std::string, std::array<char, 256>>> get_filters(c_entity);
+extern void update_filter(c_entity entity, std::string const&, std::array<char, 256> const&);
+
+extern ImGuiContext *default_context;
+
+extern component_system_manager component_system_man;
+
+struct customize_entity_comms_output_state : game_state {
+    c_entity entity;
+
+    char entity_label[256]{};
+
+    unsigned menu_flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_AlwaysAutoResize;
+
+    explicit customize_entity_comms_output_state(c_entity e) : entity(e) {
+        warp_mouse_to_center_screen();
+    }
+
+    void handle_input() override {
+        if (get_input(action_menu)->just_active) {
+            set_next_game_state(create_play_state());
+        }
+    }
+
+    void rebuild_ui() override {
+    }
+
+    void update(float dt) override {
+        if (window_has_focus() && SDL_GetRelativeMouseMode() == SDL_TRUE) {
+            SDL_SetRelativeMouseMode(SDL_FALSE);
+        }
+    }
+
+    void render(frame_data *frame) override {
+        ImGui::SetCurrentContext(default_context);
+        new_imgui_frame();
+
+        auto io = ImGui::GetIO();
+
+        // center on screen
+        ImGui::SetNextWindowPos(ImVec2{ io.DisplaySize.x / 2.0f, io.DisplaySize.y / 2.0f }, 0, ImVec2{ 0.5f, 0.5f });
+        {
+            ImGui::Begin("Comms Output", nullptr, menu_flags);
+            {
+                if (!c_entity::is_valid(entity)) {
+                    ImGui::Text("Invalid Entity %d", entity.id);
+                }
+                else {
+                    auto &type_man = component_system_man.managers.type_component_man;
+                    auto type = type_man.get_instance_data(entity);
+
+                    if (ImGui::IsWindowAppearing()) {
+                        ImGui::SetKeyboardFocusHere(0);
+                    }
+                    ImGui::Text("Set Output Label - %s", *type.name);
+                    ImGui::Separator();
+                    ImGui::Dummy(ImVec2{10, 10});
+                    bool save_label = ImGui::InputText("Comms Label", entity_label, 256, ImGuiInputTextFlags_AutoSelectAll | ImGuiInputTextFlags_EnterReturnsTrue);
+                    ImGui::Dummy(ImVec2{10, 10});
+                    save_label = ImGui::Button("Save") || save_label;
+                    if (save_label) {
+                        auto &wire_man = component_system_man.managers.wire_comms_component_man;
+                        auto wire = wire_man.get_instance_data(entity);
+
+                        free((void *) *wire.label);
+                        *wire.label = strdup(entity_label);
+                    }
+
+                    if (ImGui::Button("Back")) {
+                        set_next_game_state(create_play_state());
+                    }
+                }
+            }
+            ImGui::End();
+        }
+
+        ImGui::Render();
+    }
+};
+
+game_state *game_state::create_customize_entity_comms_output_state(c_entity e) { return new customize_entity_comms_output_state(e); }
