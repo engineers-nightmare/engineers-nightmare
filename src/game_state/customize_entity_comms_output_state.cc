02b9854315321612a7266bacba6eb1825ddc0520
Caleb Anderson <robotrising@gmail.com>
2018-04-23 16:42:17 -0600

    output labels supports children

--- src/game_state/customize_entity_comms_output_state.cc
+++ src/game_state/customize_entity_comms_output_state.cc
@@ -5,6 +5,7 @@
 #include "../imgui/imgui.h"
 #include "../input.h"
 #include "../component/component_system_manager.h"
+#include "../entity_utils.h"
 
 extern action const* get_input(en_action a);
 extern void set_next_game_state(game_state *s);
@@ -15,14 +16,41 @@ extern std::unordered_map<c_entity, std::vector<c_entity>> entity_families;
 struct customize_entity_comms_output_state : game_state {
     c_entity entity;
 
-    char entity_label[256]{};
+    std::vector<output_ui_state> entity_labels;
 
     unsigned menu_flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_AlwaysAutoResize;
 
-    explicit customize_entity_comms_output_state(c_entity e) : entity(e) {
+    std::vector<output_ui_state> get_entity_labels(c_entity entity) {
+        std::vector<output_ui_state> outputs{};
+
         auto &wire_man = component_system_man.managers.wire_comms_component_man;
-        auto wire = wire_man.get_instance_data(entity);
-        strcpy(entity_label, (*wire.label) ? *wire.label : "");
+        auto &type_man = component_system_man.managers.type_component_man;
+        if (wire_man.exists(entity)) {
+            auto wire = wire_man.get_instance_data(entity);
+            auto type = type_man.get_instance_data(entity);
+
+            outputs.emplace_back();
+            auto &f = outputs.back();
+            f.entity = entity;
+            f.component_name = *type.name;
+            if (*wire.label) {
+
+                strcpy(f.output.data(), *wire.label);
+            } else {
+                f.output[0] = '\0';
+            }
+        }
+
+        for (auto e : entity_families[entity]) {
+            auto f = get_entity_labels(e);
+            outputs.insert(outputs.end(), f.begin(), f.end());
+        }
+
+        return outputs;
+    }
+
+    explicit customize_entity_comms_output_state(c_entity e) : entity(e) {
+        entity_labels = get_entity_labels(get_root_parent(entity));
     }
 
     void handle_input() override {
@@ -49,18 +77,40 @@ struct customize_entity_comms_output_state : game_state {
                     if (ImGui::IsWindowAppearing()) {
                         ImGui::SetKeyboardFocusHere(0);
                     }
-                    ImGui::Text("Set Output Label - %s", *type.name);
-                    ImGui::Separator();
-                    ImGui::Dummy(ImVec2{10, 10});
-                    bool save_label = ImGui::InputText("Comms Label", entity_label, 256, ImGuiInputTextFlags_AutoSelectAll | ImGuiInputTextFlags_EnterReturnsTrue);
-                    ImGui::Dummy(ImVec2{10, 10});
-                    save_label = ImGui::Button("Save") || save_label;
-                    if (save_label) {
-                        auto &wire_man = component_system_man.managers.wire_comms_component_man;
-                        auto wire = wire_man.get_instance_data(entity);
 
-                        free((void *) *wire.label);
-                        *wire.label = strdup(entity_label);
+                    ImGui::Text("Set Output Label - %s", *type.name);
+
+                    ImGui::LabelText("", "%s", "Entity");
+                    ImGui::SameLine();
+                    ImGui::LabelText("", "%s", "");
+                    ImGui::SameLine();
+                    ImGui::LabelText("", "%s", "Output Label");
+
+                    int id = 0;
+                    for (auto &kvp : entity_labels) {
+                        ImGui::PushID(id++);
+                        ImGui::LabelText("", "%s", *type_man.get_instance_data(kvp.entity).name);
+
+                        ImGui::SameLine();
+                        ImGui::LabelText("", "%s", kvp.component_name.c_str());
+
+                        ImGui::SameLine();
+                        ImGui::InputText("###output", kvp.output.data(), 256);
+                        ImGui::PopID();
+                    }
+
+                    ImGui::Dummy(ImVec2{10, 10});
+
+
+                    ImGui::Dummy(ImVec2{10, 10});
+                    if (ImGui::Button("Save")) {
+                        for (auto &kvp : entity_labels) {
+                            auto &wire_man = component_system_man.managers.wire_comms_component_man;
+                            auto wire = wire_man.get_instance_data(kvp.entity);
+
+                            free((void *) *wire.label);
+                            *wire.label = strdup(kvp.output.data());
+                        }
                     }
 
                     if (ImGui::Button("Back")) {
